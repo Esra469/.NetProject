698b0591c3d3c0f59869bbfbdd58eb2bde0ebe38
EsraÇimen <eesra8352@gmail.com>
2026-03-19 00:52:32 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //product tablosunu dalını yazıyoruz. Data access layour demek.
+    public interface IProductDal
+    {
+        //data accessin entitiese bağlı olduğunu bu şekilde belirtiyoruz.Dataaccess altında dependencies altına entities geldi.
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+
+    }
+}
