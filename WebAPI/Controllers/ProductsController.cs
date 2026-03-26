510b0e3f790e97574811186c30fc15700c7447a0
EsraÇimen <eesra8352@gmail.com>
2026-03-26 17:39:56 +0300

    added startup.cs

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -11,6 +11,18 @@ namespace WebAPI.Controllers
     [ApiController] //attribute
     public class ProductsController : ControllerBase
     {
+        //loosely coupled
+        //constructor verdiğimzde aşağıdan istediğimiz gbi erişemeyiz. ama _productService dyince field ine ulaşmış oluyoruz. ordan oynama yapabiliyoruz.
+        //IoC container --Inversion of control
+        IProductService _productService;
+
+
+        //constructor oluşturduk.
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         [HttpGet]
         public List<Product> Get()
         {
@@ -19,7 +31,10 @@ namespace WebAPI.Controllers
             //    new Product{ProductId=1,ProductName="Elma"},
             //    new Product{ProductId=2,ProductName="Armut"},
             //};
-            IProductService productService = new ProductManager(new EfProductDal());
+
+            //Dependency chain 
+           // IProductService productService = new ProductManager(new EfProductDal()); //Product manager yazmadık. IProductservice yazdık çünkü referansını tutuyor.
+           
             var result =productService.GetAll();
             return result.Data;
         }
